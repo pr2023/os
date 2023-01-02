ebd9b41d149e51f60eb0b29dde298eb724b5a619
lp3 <98405181+lp-3@users.noreply.github.com>
2023-01-02 20:09:29 +0530

    Add files via upload

---
+++ Slip1_1.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+
+struct node
+{
+  int pno,freq;
+}frames[20];
+int n;
+
+int page_found(int pno)
+{
+  int fno;
+  for(fno=0;fno<n;fno++)
+	if(frames[fno].pno==pno)
+	   return fno;
+	return -1;
+}
+
+int get_free_frame()
+{  int fno;
+  for (fno=0; fno<=n; fno++)
+	if (frames[fno].pno==-1)
+		return(fno);
+   return(-1);
+}
+
+int get_lfu_frame()
+{
+  int fno;
+  int selfno=0;
+  for (fno=1; fno<n; fno++)
+	if(frames[fno].freq<frames[selfno].freq)
+	selfno=fno;
+  return selfno;
+}
+int main()
+{
+   int p_request[]={3,4,5,4,3,4,7,2,4,5,6,7,2,4,6};
+   int size=12;
+   int page_falts=0,i,j,fno;
+   
+   printf("\nHow many frames:");  scanf("%d",&n);
+   //initialize frames
+   for (i=0; i<n; i++)
+   { frames[i].pno=-1;
+	 frames[i].freq=0;
+   }
+
+   printf("\nPageNo     Page Frames          	Page Fault");
+   printf("\n---------------------------------------------------");
+   for(i=0;i<size;i++)
+   {
+	 j=page_found(p_request[i]);
+	 if(j==-1)  //page fault occurs
+	 {
+	   j=get_free_frame();
+	   if (j==-1) //no free frame - do page replacement
+		 j=get_lfu_frame();
+	   page_falts++;
+	   frames[j].pno=p_request[i];
+	   frames[j].freq=1;
+	   printf("\n%4d\t ",p_request[i]);
+	   for (fno=0; fno<n; fno++)
+		 printf("%4d:%2d",frames[fno].pno,frames[fno].freq);
+	   printf(" : YES");
+	 }
+	else //page found in frame j
+	{
+	   printf("\n%4d\t ",p_request[i]);
+	   frames[j].freq++;
+	   for (fno=0; fno<n; fno++)
+		 printf("%4d:%2d",frames[fno].pno,frames[fno].freq);
+	   printf(" : NO");
+	}
+   }
+  printf("\n-------------------------------------------------------");
+  printf("\n Number of Page_Falts=%d",page_falts);
+ // getch();
+}
+
+
+
